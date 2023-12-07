d14a7a086b8045ae93e82a765788f97d4156c53e
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2023-12-07 21:55:57 +0000

    Added Main class.

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -1,8 +1,10 @@
 package software.ulpgc.kata3;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,11 +12,36 @@ public class Main {
         List<Organization> organizations = TsvFileOrganizationLoader
                 .with(new File("organizations.tsv"))
                 .load();
-        OrganizationStatistic statistic = new FoundedOrganizationStatistic();
-        Map<String, Integer> map = statistic.calculate(organizations);
-        for (String key : map.keySet()) {
-            System.out.println(key + " = " + map.get(key));
-        }
+
+        List<Integer> employeeNumbers = organizations.stream()
+                .map(Organization::getNumberOfEmployees)
+                .collect(Collectors.toList());
+
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.histogramDisplay().show(histogram(employeeNumbers));
+        mainFrame.setVisible(true);
+
+    }
+
+    private static Histogram histogram(List<Integer> employeeNumbers) {
+        int[] employeeArray = employeeNumbers.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        return new Histogram() {
+            @Override
+            public int bins() {
+                return 17;
+            }
+
+            @Override
+            public double[] values() {
+                return Arrays.stream(employeeArray)
+                        .asDoubleStream()
+                        .toArray();
+            }
+        };
+
     }
 
 }
